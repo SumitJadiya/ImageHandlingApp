246e3776633ee036234a3cd681ae2c725c07d43f
Sumit Jadiya <sumit.jadiya@turtlemint.com>
2021-12-13 21:57:26 +0530

    setup ejs

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+const { urlencoded } = require('express')
 const express = require('express')
 
 const app = express()
+
+
+app.set('view engine', 'ejs')
+
+// middleware
+
+// to accept json data
+app.use(express.json())
+
+// to accept url encoded data
+app.use(urlencoded({extended: true}))
+
+app.get('/myget', (req, res) => {
+    
+    res.send(req.body)
+})
+
+app.get('/mygetform', (req, res) => {
+    
+    res.render("getform")
+})
+
+app.get('/mypostform', (req, res) => {
+    
+    res.render("postForm")
+})
 
 app.listen(4000, () => console.log(`Server running at 4000`))
