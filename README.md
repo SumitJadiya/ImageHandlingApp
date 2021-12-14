c04c96b533473fc21a88e0b322df31f3c02476ca
Sumit Jadiya <jadiyaskj@gmail.com>
2021-12-14 07:16:21 +0530

    upload image locally

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ImageHandlingApp
+
 App to handle forms and images using express and EJS. The images are stored in cloudinary.
+
+#### enctype="multipart/form-data"
+
+mandatory if we want to upload images
+
+#### express-fileupload plugin
+
+great plugin to handle file upload
+
+#### express.json()
+
+this middleware enables the code to accept json data
+
+#### urlencoded()
+
+this middleware allows the url-encoded-form-data
+
+#### app.set('view engine', 'ejs')
+
+to use ejs templating engine
--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const { urlencoded } = require('express')
 const express = require('express')
+const fileUpload = require('express-fileupload')
 
 const app = express()
-
 
 app.set('view engine', 'ejs')
 
 // middleware
-
 // to accept json data
 app.use(express.json())
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/"
+}))
 
 // to accept url encoded data
 app.use(urlencoded({extended: true}))
 
 app.get('/myget', (req, res) => {
+    // console.log(req.files)
     res.send(req.body)
 })
 
+app.post('/mypost', (req, res) => {
+    console.log(req.files)
+    res.send(req.body)
+})
+
+// render
 app.get('/mygetform', (req, res) => {
     res.render("getform")
 })
 
 app.get('/mypostform', (req, res) => {
-    res.render("postForm")
+    res.render("postform")
 })
 
 app.listen(4000, () => console.log(`Server running at 4000`))
